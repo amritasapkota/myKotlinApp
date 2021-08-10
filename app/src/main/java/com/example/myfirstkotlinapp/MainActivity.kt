5f6f5a126eb9b87791a315594a8ba87f438b4592
amritasapkota <asapkota81@gmail.com>
2021-08-10 12:33:13 -0400

    First commit

---
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.myfirstkotlinapp
+
+import android.content.DialogInterface
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+            showAlert()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    // build alert dialog
+    fun showAlert(){
+        val dialogBuilder = AlertDialog.Builder(this)
+        // set message of alert dialog
+        dialogBuilder.setMessage("Do you want to close this application ?")
+        // if the dialog is cancelable
+        .setCancelable(false)
+        // positive button text and action
+        .setPositiveButton("Proceed", DialogInterface.OnClickListener {
+    //                        dialog, id -> finish()
+            dialog, id -> dialog.cancel()
+        })
+        // negative button text and action
+        .setNegativeButton("Cancel", DialogInterface.OnClickListener {
+            dialog, id -> dialog.cancel()
+        })
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        alert.setTitle("AlertDialogExample")
+        // show alert dialog
+        alert.show()
+    }
+
+}
